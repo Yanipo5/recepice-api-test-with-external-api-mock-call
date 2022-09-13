dfcc4505a1cd8cfd5bd6f0199d0e58f3dea5670e
Yaniv Peretz <yaniv.peretz@gm.com>
2022-09-13 20:11:47 +0300

    add types + description

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import "dotenv/config";
-import express from "express";
-import axios from "axios";
+import 'dotenv/config';
+import express from 'express';
+import axios from 'axios';
 
-const PORT = 12990;
+const PORT = Number(process.env.PORT) || 12990;
 const app = express();
 
-app.get("/api/", (req, res) => {
-   res.json("Hello from server");
+app.get('api/greet/', (req, res) => res.json('Hello from server'));
+
+app.get('/api/greet/mock', async (req, res) => {
+  const apiResponse = await axios(process.env.EXTERNAL_API_URL);
+  res.json(`Hello from server, mock server ${apiResponse.data ? 'accepted' : 'rejected'} the request.`);
 });
 
-app.get("/api/with-3rd-party", async (req, res) => {
-   const apiResponse = await axios(process.env.EXTERNAL_API_URL);
-   res.json(`Hello from server, your request was ${apiResponse.data ? "accepted" : "rejected"} with 3rd party.`);
-});
-
-app.listen(PORT, () => console.log(`App running on ${PORT}`));
+export const server = app.listen(PORT, () => console.log(`App running on ${PORT}`));
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,19 +1,36 @@
 import _axios from 'axios';
 import { ChildProcess } from 'child_process';
 import { mock } from '../utils/mock';
+import { server } from '../src/app';
 
+/**
+ * This recepice simplify starting an app that calls to a 3rd-party.
+ * The 3rd-party response is mocked via the mock function.
+ * This example shows how the server response is effected by the mock.
+ */
 let _process: ChildProcess;
-const port = 12990;
-const axios = _axios.create({ baseURL: `http://localhost:${port}` });
+const appPort = Number(process.env.PORT) || 12990;
+const mockPort = 12999;
+const url = '/api/greet/mock';
+
+const axios = _axios.create({ baseURL: `http://localhost:${appPort}` });
 
 describe('Integration Tests', () => {
   test('Api with 3rd party call accepted', async () => {
-    const response = 'Hello from server, your request was accepted with 3rd party.';
-    const url = '/api/with-3rd-party';
-    _process = await mock([{ response, url }], { port });
-    const res = await axios(url);
-    expect(res.data).toBe(response);
+    _process = await mock([{ response: true }], { port: mockPort });
+    const res = await axios.get(url);
+    expect(res.data).toBe(`Hello from server, mock server accepted the request.`);
+  });
+
+  test('Api with 3rd party call accepted', async () => {
+    _process = await mock([{ response: false }], { port: mockPort });
+    const res = await axios.get(url);
+    expect(res.data).toBe(`Hello from server, mock server rejected the request.`);
   });
 
   afterEach(() => _process?.kill());
+
+  afterAll(async () => {
+    await new Promise(r => server.close(r));
+  });
 });
